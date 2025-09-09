6f916f6574295adcfaacc71165c2c02c10efc4b3
Akash K T <ktakash02@gmail.com>
2025-09-09 10:54:08 +0530

    Delete spi_design.sv

--- spi_design.sv
+++
@@ -1,8 +0,0 @@
-`include "spi_master_design.sv"
-`include "spi_slave_design.sv"
-
-module spi_design(input clk,rst,newd,input[11:0]din,output[11:0]dout,done);
-  wire sclk,mosi,cs;
-  spi_master m1(clk,rst,newd,din,sclk,cs,mosi);
-  spi_slave s1(sclk,cs,mosi,dout,done);
-endmodule
